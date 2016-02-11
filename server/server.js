05b7b363c64c857e8c549183259c14e415e530db
Mars Hall <mars@heroku.com>
2016-02-11 14:02:32 -0800

    Honor `PORT` environment var

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ import routes from '../common/routes'
 import { fetchCounter } from '../common/api/counter'
 
 const app = new Express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 // Use this middleware to set up hot module reloading via webpack.
 const compiler = webpack(webpackConfig)
