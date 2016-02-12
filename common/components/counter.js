eb2326981daddd242cfcaf1667877570f7ebeb30
Mars Hall <mars@heroku.com>
2016-02-11 18:41:14 -0800

    Add counter to document title.

--- common/components/counter.js
+++ common/components/counter.js
@@ -1,11 +1,18 @@
 import React, { Component, PropTypes } from 'react'
+import DocumentMeta from 'react-document-meta';
 
 class Counter extends Component {
 
   render() {
     const { increment, decrement, counter } = this.props
+    const metaData = {
+      title: `Universal App: Counter #${counter}`
+    };
+
     return (
       <p>
+        <DocumentMeta {...metaData}/>
+
         Clicked: {counter} times
         {' '}
         <button onClick={increment}>+</button>
